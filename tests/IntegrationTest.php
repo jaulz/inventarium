185c52e65a02552e5bfcf2088f772f62c44bf865
Julian Hundeloh <julian@hundeloh-consulting.ch>
2023-04-02 00:03:56 +0200

    test: add tests

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -10,117 +10,109 @@ use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 uses(RefreshDatabase::class);
 
 beforeEach(function () {
-    DB::statement('CREATE EXTENSION IF NOT EXISTS hstore');
-    DB::statement('CREATE EXTENSION IF NOT EXISTS unaccent');
+    DB::statement('CREATE EXTENSION IF NOT EXISTS pg_trgm');
 
-    $migration = include __DIR__ . '/../database/migrations/2013_01_09_141531_create_inventarium_extension.php.stub';
+    $migration = include __DIR__ . '/../database/migrations/2013_01_09_141532_create_inventarium_extension.php.stub';
     $migration->up();
 });
 
-test('creates correct slugs', function () {
+test('creates correct vectors', function () {
     Schema::create('posts', function (Blueprint $table) {
         $table->id();
         $table->text('title');
     });
 
     Schema::table('posts', function (Blueprint $table) {
-        $table->inventarium('slug', 'title');
+        $table->inventarium('title', 'A');
     });
 
     collect([
-        'test' => 'test',
-        'Test-Story-4' => 'test-story-4',
-        'äöü' => 'aeoeue',
-        'èô' => 'eo'
-    ])->each(function ($key, $value) {
+        'a fat cat sat on a mat and ate a fat rat' => "'a':1,6,10 'and':8 'ate':9 'cat':3 'fat':2,11 'mat':7 'on':5 'rat':12 'sat':4",
+    ])->each(function ($value, $key) {
         $post = DB::table('posts')->insertReturning([
-            'title' => $value
+            'title' => $key
         ])->first();
 
-        expect($post->slug)->toBe($key);
+        expect($post->vectors)->toBe($value);
     });
 });
 
-test('updates correct slugs', function () {
+test('creates correct trigrams', function () {
     Schema::create('posts', function (Blueprint $table) {
         $table->id();
         $table->text('title');
     });
 
     Schema::table('posts', function (Blueprint $table) {
-        $table->inventarium('slug', 'title');
+        $table->inventarium('title', 'A');
     });
 
     collect([
-        'test' => 'test',
-        'Test-Story-4' => 'test-story-4',
-        'äöü' => 'aeoeue',
-        'èô' => 'eo'
-    ])->each(function ($key, $value) {
-         DB::table('posts')->insert([
-            'title' => '---'
-        ]);
-
-        $post = DB::table('posts')->updateReturning([
-            'title' => $value
-        ])->first();
-
-        expect($post->slug)->toBe($key);
-    });
-});
-
-test('increments suffix when same slug is used multiple times', function () {
-    Schema::create('posts', function (Blueprint $table) {
-        $table->id();
-        $table->text('title');
-    });
-
-    Schema::table('posts', function (Blueprint $table) {
-        $table->inventarium('slug', 'title');
-    });
-
-    collect([null,null,null,null])->keys()->each(function($index) {
+        'a fat cat sat on a mat and ate a fat rat' => '{"  a","  c","  f","  m","  o","  r","  s"," a "," an"," at"," ca"," fa"," ma"," on"," ra"," sa",and,"at ",ate,cat,fat,mat,"nd ","on ",rat,sat,"te "}',
+    ])->each(function ($value, $key) {
         $post = DB::table('posts')->insertReturning([
-            'title' => 'test'
+            'title' => $key
         ])->first();
 
-        $suffix = $index > 0 ? '_' . ($index + 1) : '';
-        expect($post->slug)->toBe('test' . $suffix);
+        expect($post->trigrams)->toBe($value);
     });
 });
 
-test('remembers slugs once assigned', function () {
+test('can use different languages', function () {
     Schema::create('posts', function (Blueprint $table) {
         $table->id();
         $table->text('title');
+        $table->text('language_code');
     });
 
     Schema::table('posts', function (Blueprint $table) {
-        $table->inventarium('slug', 'title');
+        $table->inventarium('title', 'A', 'language_code');
     });
 
-    $initialTitle = 'test';
-    $initialSlug = 'test';
+    collect([
+        'xx' => [
+            'title' => 'The Fat Rats',
+            'vectors' => "'fat':2 'rats':3 'the':1",
+            'trigrams' => '{"  f","  r","  t"," fa"," ra"," th","at ",ats,fat,"he ",rat,the,"ts "}',
+        ],
+        'en' => [
+            'title' => 'The Fat Rats',
+            'vectors' => "'fat':2 'rat':3",
+            'trigrams' => '{"  f","  r"," fa"," ra","at ",fat,rat}',
+        ],
+        'de' => [
+            'title' => 'Die fetten Ratten',
+            'vectors' => "'fett':2 'ratt':3",
+            'trigrams' => '{"  f","  r"," fe"," ra",att,ett,fet,rat,"tt "}',
+        ],
+    ])->each(function ($value, $key) {
+        $post = DB::table('posts')->insertReturning([
+            'title' => $value['title'],
+            'language_code' => $key,
+        ])->first();
 
-    $initialPost = DB::table('posts')->insertReturning([
-        'title' => $initialTitle
-    ])->first();
-    expect($initialPost->slug)->toBe($initialSlug);
-
-    $initialPost = DB::table('posts')->updateReturning([
-        'title' => 'not a test anymore'
-    ])->first();
-    expect($initialPost->slug)->toBe('not-a-test-anymore');
-
-    $secondPost = DB::table('posts')->insertReturning([
-        'title' => 'test'
-    ])->first();
-    expect($secondPost->slug)->toBe('test_2');
-
-    $initialPost = DB::table('posts')->where([
-        'id' => $initialPost->id,
-    ])->updateReturning([
-        'title' => 'test'
-    ])->first();
-    expect($initialPost->slug)->toBe('test');
+        expect($post->vectors)->toBe($value['vectors']);
+        expect($post->trigrams)->toBe($value['trigrams']);
+    });
 });
+
+/*test('works with JSON columns', function () {
+    Schema::create('posts', function (Blueprint $table) {
+        $table->id();
+        $table->jsonb('title');
+    });
+
+    Schema::table('posts', function (Blueprint $table) {
+        $table->inventarium("array_to_string(jsonb_values(title), ' ')", 'A');
+    });
+
+    collect([
+        'a fat cat sat on a mat and ate a fat rat' => "'a':1,6,10 'and':8 'ate':9 'cat':3 'fat':2,11 'mat':7 'on':5 'rat':12 'sat':4",
+    ])->each(function ($value, $key) {
+        $post = DB::table('posts')->insertReturning([
+            'title' => $key
+        ])->first();
+
+        expect($post->vectors)->toBe($value);
+    });
+});*/
